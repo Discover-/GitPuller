4bddbb44fe190ccfac0a670c62d3294e7a0a6e8c
Discover- <amort11@hotmail.com>
2014-03-16 13:47:53 +0100

    Add empty project

---
+++ GitPuller/GitPuller/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace GitPuller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
